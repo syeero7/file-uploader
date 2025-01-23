875a66c30e92a021047a2d514c8bdddec3fd3de0
syeero7 <EmalHasitha@proton.me>
2025-01-23 18:02:34 +0530

    feat: add queries for folder CRUD operations

--- prisma/queries.js
+++ prisma/queries.js
@@ -10,3 +10,37 @@ export const createUser = async (email, password) => {
     data: { email, password, folders: { create: { name: "/" } } },
   });
 };
+
+export const getFoldersByUserId = async (ownerId) => {
+  const folders = await prisma.folder.findMany({ where: { ownerId } });
+  return folders;
+};
+
+export const createFolder = async (ownerId, foldername) => {
+  await prisma.folder.create({
+    data: { name: foldername, owner: { connect: { id: ownerId } } },
+  });
+};
+
+export const getFolder = async (ownerId, folderUniqueIdentifier) => {
+  const folder = await prisma.folder.findFirst({
+    where: { ...folderUniqueIdentifier, ownerId },
+  });
+  return folder;
+};
+
+export const getFoldersWithFilesByUserId = async (ownerId) => {
+  const folders = await prisma.folder.findMany({
+    where: { ownerId },
+    include: { files: true },
+  });
+  return folders;
+};
+
+export const deleteFolder = async (folderId) => {
+  await prisma.folder.delete({ where: { id: folderId } });
+};
+
+export const renameFolder = async (folderId, name) => {
+  await prisma.folder.update({ where: { id: folderId }, data: { name } });
+};
